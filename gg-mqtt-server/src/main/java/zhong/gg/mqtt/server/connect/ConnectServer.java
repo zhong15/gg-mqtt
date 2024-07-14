d735038e7d9135c99c9f874552adcb1de111a97b
zhong <zhenglouzhong@icloud.com>
2024-07-14 22:35:33 +0800

    feat: 添加 mqtt-server 的接口

---
+++ gg-mqtt-server/src/main/java/zhong/gg/mqtt/server/connect/ConnectServer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zhong.gg.mqtt.server.connect;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.MqttConnectMessage;
+import io.netty.handler.codec.mqtt.MqttConnectVariableHeader;
+import zhong.gg.mqtt.server.Server;
+
+/**
+ * @author Zhong
+ * @since 0.0.1
+ */
+public interface ConnectServer extends Server {
+    void putClient(String clientId, Channel channel);
+
+    Channel removeClient(String clientId);
+
+    Channel getClient(String clientId);
+
+    void putConnectMessage(String clientId, MqttConnectMessage msg);
+
+    MqttConnectMessage removeConnectMessage(String clientId);
+
+    MqttConnectMessage getConnectMessage(String clientId);
+
+    void putSession(String clientId, Session session);
+
+    Session removeSession(String clientId);
+
+    Session getSession(String clientId);
+
+    void refreshTime(String clientId);
+
+    Long removeRefreshTime(String clientId);
+
+    Long getRefreshTime(String clientId);
+
+    void checkKeepAlive();
+}
---
+++ gg-mqtt-server/src/main/java/zhong/gg/mqtt/server/protocol/SubscribeAction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (ChannelHandlerContext ctx, MqttMessage msgthe "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zhong.gg.mqtt.server.protocol;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.mqtt.MqttSubAckMessage;
+import io.netty.handler.codec.mqtt.MqttSubscribeMessage;
+import io.netty.handler.codec.mqtt.MqttUnsubscribeMessage;
+
+/**
+ * @author Zhong
+ * @since 0.0.1
+ */
+public interface SubscribeAction {
+    Object onSubscribe(ChannelHandlerContext ctx, MqttSubscribeMessage msg);
+
+    Object onSubAck(ChannelHandlerContext ctx, MqttSubAckMessage msg);
+
+    Object onUnsubscribe(ChannelHandlerContext ctx, MqttUnsubscribeMessage msg);
+
+    Object onUnsubAck(ChannelHandlerContext ctx, MqttUnsubscribeMessage msg);
+}
